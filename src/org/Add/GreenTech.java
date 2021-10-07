eb37f4575f375c5dc3f75531e744707a8174fc54
vinodha <vinog944@gmail.com>
2021-10-07 19:46:45 +0530

    added java

---
+++ src/org/Add/GreenTech.java
@@ -0,0 +1,42 @@
+package org.Add;
+
+public class GreenTech {
+private void greensOmr() {
+	System.out.println("Green Tech OMR: No:1 East, Chennai");
+
+}
+private void greensAdayar() {
+	System.out.println("Greens Adayar: No:2 South, Chennai");
+
+}
+private void greensTambaram() {
+	System.out.println("Green Tambaram: No:3 West, Chennai");
+
+}
+private void greensVelachery() {
+	System.out.println("Green Velachery: No:4 North, Chennai");
+
+}
+	private void greensAnnanagar() {
+		System.out.println("Greens Anna Nagar: No:5 NorthEast, Chennai");
+
+	}
+	
+	public static void main(String[] args) {
+		GreenTech e = new GreenTech();
+		e.greensAdayar();
+		e.greensAnnanagar();
+		e.greensOmr();
+		e.greensTambaram();
+		e.greensVelachery();
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
